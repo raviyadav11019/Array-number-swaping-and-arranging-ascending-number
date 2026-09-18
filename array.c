65e9dbbf9e54ca71bf1378b37cf944f06ad67d24
Ravi Kumar Yadav <raviyadav11019@gmail.com>
2026-09-18 16:12:27 +0000

    code not done yet

---
+++ array.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int i,j;
+    int arr[i];
+    printf("Enter Your Number");
+    for(i=0;i<=5;i++){
+        scanf("%d",&i);
+
+    }
+    printf("%d",arr[i]);
+} 
